739b3bbc16eacb341c740c6e28085dc06fb20327
Kristina <135939684+RivaelManurung@users.noreply.github.com>
2025-03-25 21:46:55 +0700

    web

--- Website TSTH2/resources/views/Master/Layout/app.blade.php
+++ Website TSTH2/resources/views/Master/Layout/app.blade.php
@@ -8,86 +8,60 @@
 	<title>Limitless - Responsive Web Application Kit by Eugene Kopyov</title>
 
 	<!-- Global stylesheets -->
-	<link href="{{ asset('global_assets/fonts/inter/inter.css') }}" rel="stylesheet" type="text/css">
-	<link href="{{ asset('global_assets/icons/phosphor/styles.min.css') }}" rel="stylesheet" type="text/css">
-	<link href="{{ asset('global_assets/css/ltr/all.min.css') }}" id="stylesheet" rel="stylesheet" type="text/css">
+	<link href="{{ asset('assets/fonts/inter/inter.css') }}" rel="stylesheet" type="text/css">
+	<link href="{{ asset('assets/icons/phosphor/styles.min.css') }}" rel="stylesheet" type="text/css">
+	<link href="{{ asset('assets/css/ltr/all.min.css') }}" id="stylesheet" rel="stylesheet" type="text/css">
 	<!-- /global stylesheets -->
 
 	<!-- Core JS files -->
-	<script src="{{ asset('global_assets/demo/demo_configurator.js') }}"></script>
-	<script src="{{ asset('global_assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
+	<script src="{{ asset('assets/demo/demo_configurator.js') }}"></script>
+	<script src="{{ asset('assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
 	<!-- /core JS files -->
 
 	<!-- Theme JS files -->
-	<script src="{{ asset('global_assets/js/vendor/visualization/d3/d3.min.js') }}"></script>
-	<script src="{{ asset('global_assets/js/vendor/visualization/d3/d3_tooltip.js') }}"></script>
+	<script src="{{ asset('assets/js/vendor/visualization/d3/d3.min.js') }}"></script>
+	<script src="{{ asset('assets/js/vendor/visualization/d3/d3_tooltip.js') }}"></script>
 
-	<script src="{{ asset('global_assets/js/app.js') }}"></script>
-	<script src="{{ asset('global_assets/demo/pages/dashboard.js') }}"></script>
-	<script src="{{ asset('global_assets/demo/charts/pages/dashboard/streamgraph.js') }}"></script>
-	<script src="{{ asset('global_assets/demo/charts/pages/dashboard/sparklines.js') }}"></script>
-	<script src="{{ asset('global_assets/demo/charts/pages/dashboard/lines.js') }}"></script>
-	<script src="{{ asset('global_assets/demo/charts/pages/dashboard/areas.js') }}"></script>
-	<script src="{{ asset('global_assets/demo/charts/pages/dashboard/donuts.js') }}"></script>
-	<script src="{{ asset('global_assets/demo/charts/pages/dashboard/bars.js') }}"></script>
-	<script src="{{ asset('global_assets/demo/charts/pages/dashboard/progress.js') }}"></script>
-	<script src="{{ asset('global_assets/demo/charts/pages/dashboard/heatmaps.js') }}"></script>
-	<script src="{{ asset('global_assets/demo/charts/pages/dashboard/pies.js') }}"></script>
-	<script src="{{ asset('global_assets/demo/charts/pages/dashboard/bullets.js') }}"></script>
+	<script src="{{ asset('assets/js/app.js') }}"></script>
+	<script src="{{ asset('assets/demo/pages/dashboard.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/streamgraph.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/sparklines.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/lines.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/areas.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/donuts.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/bars.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/progress.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/heatmaps.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/pies.js') }}"></script>
+	<script src="{{ asset('assets/demo/charts/pages/dashboard/bullets.js') }}"></script>
+	<script src="{{ asset('assets/js/bootstrap/bootstrap.bundle.min.js') }}"></script>
+
 	<!-- /theme JS files -->
 </head>
 
 <body>
 
-{{-- 
-	<!-- Include Navbar -->
-	@include('Master.Layout.navbar')
-
-	<div class="page-content">
-		<!-- Include Header -->
-		@include('Master.Layout.sidebar')
-
-		@include('Master.Layout.header')
 
 
+    @include('Master.Layout.navbar')
 
-		<div class="body-wrapper-inner">
-			<div class="container-fluid">
-				<main>
-					<!-- Include dynamic content -->
-					@yield('content')
-				</main>
-			</div>
-		</div>
-	</div> --}}
-	<!-- Main Navbar -->
-    <div class="navbar navbar-dark navbar-expand-lg navbar-static border-bottom border-bottom-white border-opacity-10">
-        @include('Master.Layout.navbar')
-    </div>
-
-    <!-- Page Content -->
     <div class="page-content d-flex">
+        
+        @include('Master.Layout.sidebar')
 
-        <!-- Sidebar -->
-        <div class="sidebar sidebar-dark sidebar-main sidebar-expand-lg">
-            <div class="sidebar-content">
-                <!-- Sidebar Header -->
-                <div class="sidebar-section">
-                    @include('Master.Layout.sidebar')
-                </div>
-            </div>
-        </div>
-
-        <!-- Main Content -->
-        <div class="content-wrapper flex-fill">
+        <div class="content-wrapper">
             <div class="content-inner">
-                <div class="container-fluid py-3">
+                <!-- **Pastikan ini ada** -->
+                @include('Master.Layout.header')
+
+                <div class="content">
                     @yield('content')
                 </div>
             </div>
         </div>
-        
     </div>
+
+	</div>
 
 
 </body>
